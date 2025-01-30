01f5ca9c35540faa7b3dcf530c6b64b0dcf98d41
BRAMMESWARAN V <127662344+Brammes2000@users.noreply.github.com>
2025-01-30 21:24:48 +0530

    Pattern.java

--- Que6/Pattern.java
+++ Que6/Pattern.java
@@ -1,22 +1,23 @@
 package Que6;
 
 public class Pattern {
-    public static void main(String[] args) {
-		        int num = 5;
-		        
-		        for (int i = 1; i <= num; i++) {
-		            int range = 5;
-		            
-		            for (int j = 1; j <= i; j++) {
-		                System.out.print(range + " ");
-		                range--;
-		            }
-		            
-		            for (int j = i; j < num; j++) {
-		                System.out.print(range + " ");
-		            }
-		            
-		            System.out.println();
-		        }
-		    }
-		}
+	 public static void main(String[] args) {
+	        int rows = 5;
+	        
+	        for (int i = 1; i <= rows; i++) {
+	            int num = 6;
+	            
+	            for (int j = 1; j <= i; j++) {
+	                
+	                num--;
+	                System.out.print(num + " ");
+	            }
+	            
+	            for (int j = i; j < rows; j++) {
+	                System.out.print(num + " ");
+	            }
+	            
+	            System.out.println();
+	        }
+	    }
+	}
